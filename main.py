e661ffdf8f9d0bd6178368a26d8db724a64ba82d
PerovAndrey <nikita.vasiliev1982@mail.ru>
2024-07-26 18:26:10 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+print("Hello world")
+print(12+89-1)
+# "1st program"
+print(9**0.5*5)
+# "2nd program"
+print(9.99>9.98 and 1000!=1100.1)
+# "3rd program"
+print((1234%1000//10)+(5678%1000//10))
+# "4th program"
+print((13.42*100%100) == (42.13//1) or (13.42//1) == (42.13*100%100))
